94e538243613951fe800ab1f325e74918e2c6c3c
nlovendahl <nisselovendahl@gmail.com>
2018-01-12 14:18:20 -0500

    Fix js link

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,11 @@
-Yo
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
